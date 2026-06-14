5c9f2705ec31f17775007f535c96c3c53fe871cb
AsanJayarathne <asanjayarathne@gmail.com>
2026-06-14 14:56:58 +0530

    Add login page

--- distributor-frontend/src/routes/AppRoutes.jsx
+++ distributor-frontend/src/routes/AppRoutes.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import Login from "../auth/Login";
 import DistributorLayout from "../layout/DistributorLayout";
-
 import Dashboard from "../pages/Dashboard";
 
 export default function AppRoutes() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path="/" element={<DistributorLayout />}>
         <Route index element={<Dashboard />} />
       </Route>
