7d6eb66a82f36e7d97b7f4f90461b824f4c45d43
H.M.T.I.A.Bandara <tharinduanupamati8@gmail.com>
2026-06-21 22:28:52 +0530

    add products page

---
+++ retailer-frontend/src/pages/Products.jsx
@@ -0,0 +1,87 @@
+import { useContext, useState } from "react";
+
+import products from "../data/products";
+import { CartContext } from "../context/CartContextObject";
+import SearchBar from "../components/products/SearchBar";
+import CategoryFilter from "../components/products/CategoryFilter";
+import ProductGrid from "../components/products/ProductGrid";
+import TrendingProducts from "../components/products/TrendingProducts";
+import RecommendedProducts from "../components/products/RecommendedProducts";
+import ProductDetailsModal from "../components/products/ProductDetailsModal";
+import AddToCartModal from "../components/products/AddToCartModal";
+
+function Products() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [cartProduct, setCartProduct] = useState(null);
+
+  const { addToCart } = useContext(CartContext);
+
+  const filteredProducts = products.filter((product) => {
+    const matchesCategory =
+      selectedCategory === "All" ||
+      product.category === selectedCategory;
+
+    const matchesSearch =
+      product.name
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase());
+
+    return matchesCategory && matchesSearch;
+  });
+
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-6">
+        Products
+      </h1>
+
+
+      <TrendingProducts
+        products={products}
+        onView={setSelectedProduct}
+        onCart={setCartProduct}
+      />
+
+      <RecommendedProducts
+        products={products}
+        onView={setSelectedProduct}
+        onCart={setCartProduct}
+      />
+
+      <h2 className="text-xl font-bold mb-6">
+        All Products
+      </h2>
+
+      <SearchBar
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+      />
+
+      <CategoryFilter
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
+
+      <ProductGrid
+        products={filteredProducts}
+        onView={setSelectedProduct}
+        onCart={setCartProduct}
+      />
+
+      <ProductDetailsModal
+        product={selectedProduct}
+        onClose={() => setSelectedProduct(null)}
+      />
+
+      <AddToCartModal
+        product={cartProduct}
+        onClose={() => setCartProduct(null)}
+        onConfirm={addToCart}
+      />
+    </div>
+  );
+}
+
+export default Products;
