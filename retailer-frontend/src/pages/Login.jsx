36b60f6462ef66791921d7a2ee476c7e10ea1ffd
AsanJayarathne <asanjayarathne@gmail.com>
2026-09-20 23:33:23 +0530

    fix ui

--- retailer-frontend/src/pages/Login.jsx
+++ retailer-frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { FiMail, FiLock, FiEye, FiEyeOff } from "react-icons/fi";
+import { FiMail, FiLock, FiEye, FiEyeOff, FiGlobe } from "react-icons/fi";
 import loginImage from "../assets/images/shop.png"; 
 import { useAuth } from "../context/AuthContext";
 import { useLanguage } from "../context/LanguageContext";
